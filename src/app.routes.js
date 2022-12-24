16b65f6c8821e1aed3ac2cd75b782357624ef58d
chemokita13 <chemokita13@gmail.com>
2022-12-24 14:44:48 +0100

    routes added

--- src/app.routes.js
+++ src/app.routes.js
@@ -1,9 +1,17 @@
-import { Router } from "express";
+import { Router } from "express"; //expres router
+import { sender } from "./nodemailer.js";
 
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.json("hola");
+    res.json("server working");
+});
+
+router.post("/send", (req, res) => {
+    res.json(req.body);
+    const { name, email, content } = req.body;
+    // Because phone is an optional param
+    sender(name, req.body.phone ? req.body.phone : "none", email, content);
 });
 
 export default router;
